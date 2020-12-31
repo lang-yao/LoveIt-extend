61e852fa0d8e3bc9fd8516dd6c27b23ab9e48c1e
langyao <langyaocc@163.com>
2020-12-31 10:38:27 +0800

    delete

--- en/basic-markdown-syntax/index.md
+++
@@ -1,8 +0,0 @@
-# Basic Markdown Syntax
-
-
-This article offers a sample of basic Markdown syntax that can be used in Hugo content files.
-
-<!--more-->
-## content.
-
--- js/post-single.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	if ($('#rewardButton')[0]) {
-	    $("#rewardButton").on("click", function() {
-	       $('#QR').slideToggle(); 
-	    });
-	}
-});
